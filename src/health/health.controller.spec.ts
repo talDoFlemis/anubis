c13bc4b76e44246eee9a40248c735876f5af1ad8
Said Rodrigues <coderflemis@gmail.com>
2026-03-12 07:33:25 -0300

    fix: tests

--- src/health/health.controller.spec.ts
+++ src/health/health.controller.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { HealthCheckService, HttpHealthIndicator } from '@nestjs/terminus';
 import { HealthController } from './health.controller';
+import { DrizzleDBHealthIndicator } from 'src/database/drizzle.health';
+import { MailHealthIndicator } from 'src/mail/mail.health';
 
 describe('HealthController', () => {
   let controller: HealthController;
@@ -7,6 +10,24 @@ describe('HealthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [HealthController],
+      providers: [
+        {
+          provide: HealthCheckService,
+          useValue: { check: jest.fn() },
+        },
+        {
+          provide: HttpHealthIndicator,
+          useValue: { pingCheck: jest.fn() },
+        },
+        {
+          provide: DrizzleDBHealthIndicator,
+          useValue: { isHealthy: jest.fn() },
+        },
+        {
+          provide: MailHealthIndicator,
+          useValue: { isHealthy: jest.fn() },
+        },
+      ],
     }).compile();
 
     controller = module.get<HealthController>(HealthController);
