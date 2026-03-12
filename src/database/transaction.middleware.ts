92d348042876148c005916fab21dfd9d1797dcab
Said Rodrigues <coderflemis@gmail.com>
2026-03-12 17:49:27 -0300

    feat(transaction.middleware): add aborted and close handle

--- src/database/transaction.middleware.ts
+++ src/database/transaction.middleware.ts
@@ -28,6 +28,14 @@ export class TransactionMiddleware implements NestMiddleware {
             reject(err);
           });
 
+          res.on('aborted', (err: Error) => {
+            reject(err);
+          });
+
+          res.on('close', (err: Error) => {
+            reject(err);
+          });
+
           next();
         });
       });
