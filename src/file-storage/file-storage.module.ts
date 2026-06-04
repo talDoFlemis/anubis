aa7d1126a65738469de72042fc962f1852ff2628
Gabrigas <artroxgabriel@gmail.com>
2026-06-04 15:06:28 -0300

    feat: dynamically provide STORAGE_DRIVER in FileStorageModule

--- src/file-storage/file-storage.module.ts
+++ src/file-storage/file-storage.module.ts
@@ -1,27 +1,16 @@
-import { S3Client } from '@aws-sdk/client-s3';
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { S3_CLIENT } from './file-storage.constants';
+import { S3StorageDriver } from './drivers/s3-storage.driver';
+import { STORAGE_DRIVER } from './drivers/storage-driver.interface';
 import { FileStorageService } from './file-storage.service';
 import { FileStorageDrizzlePersistenceModule } from './infrastructure/persistence/drizzle/drizzle-persistence.module';
 
 @Module({
   imports: [FileStorageDrizzlePersistenceModule],
   providers: [
+    S3StorageDriver,
     {
-      provide: S3_CLIENT,
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService): S3Client => {
-        return new S3Client({
-          endpoint: configService.getOrThrow<string>('S3_ENDPOINT'),
-          region: configService.getOrThrow<string>('S3_REGION'),
-          credentials: {
-            accessKeyId: configService.getOrThrow<string>('S3_ACCESS_KEY_ID'),
-            secretAccessKey: configService.getOrThrow<string>('S3_SECRET_ACCESS_KEY'),
-          },
-          forcePathStyle: configService.get<boolean>('S3_FORCE_PATH_STYLE', true),
-        });
-      },
+      provide: STORAGE_DRIVER,
+      useExisting: S3StorageDriver,
     },
     FileStorageService,
   ],
