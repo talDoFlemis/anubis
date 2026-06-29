3fa923444eae0bbe7de34e3017a6c838a8860d2b
Wanderson Soares <wandersonsoares841@gmail.com>
2026-06-29 19:12:29 -0300

    feat: add classification and interview modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,15 +8,15 @@ import { AuthEmailModule } from './auth-email/auth-email.module';
 import { AuthGoogleModule } from './auth-google/auth-google.module';
 import { AuthModule } from './auth/auth.module';
 import { CandidateModule } from './candidate/candidate.module';
+import { ClassificationModule } from './classification/classification.module';
 import { LoggingModule } from './common/logging.module';
 import { HttpLoggerMiddleware } from './common/middlewares/http-logger.middleware';
 import { CvScoringModule } from './cv-scoring/cv-scoring.module';
-import { ValidationModule } from './validation/validation.module';
-
 import { DatabaseModule } from './database/database.module';
 import { EnrollmentModule } from './enrollment/enrollment.module';
 import { validate } from './env.validation';
 import { HealthModule } from './health/health.module';
+import { InterviewModule } from './interview/interview.module';
 import { MailModule } from './mail/mail.module';
 import { ProfessorModule } from './professor/professor.module';
 import { ResearchThemeModule } from './research-theme/research-theme.module';
@@ -25,6 +25,7 @@ import { SessionModule } from './session/session.module';
 import { SystemModule } from './system/system.module';
 import { UniversityModule } from './university/university.module';
 import { UsersModule } from './users/users.module';
+import { ValidationModule } from './validation/validation.module';
 
 @Module({
   imports: [
@@ -48,6 +49,8 @@ import { UsersModule } from './users/users.module';
     SecretaryModule,
     CandidateModule,
     UniversityModule,
+    InterviewModule,
+    ClassificationModule,
     AuthModule,
     AuthEmailModule,
     AuthGoogleModule,
@@ -63,6 +66,7 @@ import { UsersModule } from './users/users.module';
   providers: [AppService],
 })
 export class AppModule implements NestModule {
+  constructor() {}
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(HttpLoggerMiddleware).forRoutes('*');
   }
