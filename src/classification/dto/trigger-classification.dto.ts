23360c85756a36bb8e8a224ce64579e30967d146
Wanderson Soares <wandersonsoares841@gmail.com>
2026-07-08 09:12:32 -0300

    fix(classification): replace IsUUID with IsString in dto

--- src/classification/dto/trigger-classification.dto.ts
+++ src/classification/dto/trigger-classification.dto.ts
@@ -1,4 +1,4 @@
-import { IsEnum, IsOptional, IsUUID } from 'class-validator';
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 
 export enum StageEnum {
   MESTRADO = 'mestrado',
@@ -7,7 +7,7 @@ export enum StageEnum {
 
 export class TriggerClassificationDto {
   @IsOptional()
-  @IsUUID()
+  @IsString()
   researchThemeId?: string;
 
   @IsOptional()
